e65ce22dcc869ead24253e4235538cff205c2608
gliberov <124058342+gliberov@users.noreply.github.com>
2024-05-08 13:13:10 -0400

    yes

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -13,7 +13,13 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('../views/Home.vue')
     }
+
   ]
 })
 
