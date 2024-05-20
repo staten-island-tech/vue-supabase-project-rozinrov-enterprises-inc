0b2521109d1c92cfb6a66975507c95a0e8092aad
Nosha08 <noah.rozin@outlook.com>
2024-05-20 00:14:35 -0400

    a

--- vite-project/src/views/Maps.vue
+++ vite-project/src/views/Maps.vue
@@ -1,16 +1,41 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { supabase } from '../lib/supabaseClient'
 
-const location = ref('')
+import { Loader } from "@googlemaps/js-api-loader"
 
-console.log(location.value)
+
+const loader = new Loader({
+  apiKey: "AIzaSyDALWA-g2AFNDsDyYFlo43-1mjrP3KsoL4",
+});
+
+let map: google.maps.Map;
+
+
+loader.load().then(async () => {
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  map = new Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+});
 
 </script>
 
 <template>
     <div>
         <h1>Google Maps Integration Test</h1>
-        <input type="text" v-model="location" placeholder="Input Location" />
+        <input type="text" placeholder="Input Location" />
+        <div id="map"></div>
     </div>
 </template>
+
+<style>
+  #map {
+    height: 100%;
+  }
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+</style>
