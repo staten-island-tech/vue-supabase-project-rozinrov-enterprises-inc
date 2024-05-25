dff2e7c5a1b19a4d8ef80b54762e1d9fd512fc65
Nosha08 <noah.rozin@outlook.com>
2024-05-25 16:22:55 -0400

    a

--- vite-project/src/views/Maps.vue
+++ vite-project/src/views/Maps.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { supabase } from '../lib/supabaseClient'
-
 import { Loader } from "@googlemaps/js-api-loader"
-
 
 const loader = new Loader({
   apiKey: "AIzaSyDALWA-g2AFNDsDyYFlo43-1mjrP3KsoL4",
@@ -13,6 +11,7 @@ let mapDiv = ref(null)
 let map = ref(null)
 let panorama = ref(null)
 let submitVisible = ref(false)
+let submitForm = ref(() => {})
 
 onMounted(async () => {
     await loader.load()
@@ -33,12 +32,13 @@ onMounted(async () => {
         })
 
         window.google.maps.event.addListener(panorama.value, 'visible_changed', function() {
-          const submit = document.querySelector('#submit')
           submitVisible.value = true
-          submit.addEventListener('submit', function() {
-            console.log('Submitted')
-          })
         })
+
+        submitForm.value = function() {
+          console.log("Submitted")
+        }
+
       })
     }
     catch (error) {
@@ -54,7 +54,9 @@ onMounted(async () => {
         <div id="content">
             <h1>Google Maps Integration Test</h1>
             <input type="text" placeholder="Input Location" />
-            <input type="submit" id="submit" placeholder="Submit" v-if="submitVisible" />
+            <form @submit.prevent="submitForm">
+              <input type="submit" id="submit" placeholder="Submit" v-if="submitVisible" />
+            </form>
         </div>
     </div>
 </template>
@@ -78,6 +80,7 @@ onMounted(async () => {
     flex-direction: column;
     align-items: center;
     pointer-events: none;
+    z-index: 1;
   }
 
   #content > * {
