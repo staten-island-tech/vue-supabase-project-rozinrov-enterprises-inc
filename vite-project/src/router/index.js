839e57dcd17ee741fa051fe7ec96f99d9450f513
gliberov <124058342+gliberov@users.noreply.github.com>
2024-06-06 12:57:52 -0400

    e

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -34,7 +34,6 @@ const router = createRouter({
       path: '/post',
       name: 'post',
       component: () => import('../views/Post.vue')
-      component: () => import('../views/Guess1.vue')
     }
 
   ]
